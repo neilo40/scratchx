830d38cb1b9fb5182d6bb22f96384526dd7a9352
neil <neil@neiloconnor.com>
2017-12-06 22:35:36 +0000

    add other move actions

--- doomLink.js
+++ doomLink.js
@@ -12,13 +12,41 @@ new (function() {
         return {status: 2, msg: 'Ready'};
     };
 
-    ext.move_forward = function(callback) {
-        console.log("Moving forward");
+    var post_action = function(action_type) {
         var xmlHttp = new XMLHttpRequest();
         xmlHttp.open("POST", "http://localhost:6001/api/player/actions", true); 
-	xmlHttp.setRequestHeader("Content-type", "application/json");
-        var data = JSON.stringify({"type": "forward"});
-	xmlHttp.send(data);
+        xmlHttp.setRequestHeader("Content-type", "application/json");
+        var data = JSON.stringify({"type": action_type});
+        xmlHttp.send(data);
+    }
+
+    ext.move_forward = function(callback) {
+        console.log("Moving forward");
+        post_action("forward");
+        callback();
+    };
+
+    ext.move_backward = function(callback) {
+        console.log("Moving backward");
+        post_action("backward");
+        callback();
+    };
+
+    ext.turn_right = function(callback) {
+        console.log("Turning right");
+        post_action("turn-right");
+        callback();
+    };
+
+    ext.turn_left = function(callback) {
+        console.log("Turning left");
+        post_action("turn-left");
+        callback();
+    };
+
+    ext.shoot = function(callback) {
+        console.log("Shooting");
+        post_action("shoot");
         callback();
     };
 
@@ -26,6 +54,10 @@ new (function() {
     var descriptor = {
         blocks: [
             ['w', 'move forward', 'move_forward'],
+            ['w', 'move backward', 'move_backward'],
+            ['w', 'turn right', 'turn_right'],
+            ['w', 'turn left', 'turn_left'],
+            ['w', 'shoot', 'shoot']
         ]
     };
 
